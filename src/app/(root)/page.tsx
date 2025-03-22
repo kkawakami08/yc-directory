5c582d8f5f863e14e9ea383f3ad96124fe599a65
Kaori Kawakami <k.kawakami08@gmail.com>
2025-03-22 14:19:39 -0700

    updated tailwind custom classes and fixed some tailwind errors

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,10 +1,21 @@
 import React from "react";
+import SearchForm from "../components/search-form";
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>HomePage</h1>
-    </div>
+    <>
+      <section className="pink-container ">
+        {" "}
+        <h1 className="heading">
+          Pitch Your Startup, <br /> Connect With Entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+          Competitions.
+        </p>
+        <SearchForm />
+      </section>
+    </>
   );
 };
 
